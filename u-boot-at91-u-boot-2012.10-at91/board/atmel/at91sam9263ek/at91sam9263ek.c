f26a9db8036de8b8e1dfecf493561616e26d9de6
ponyo1983 <conding4fun@126.com>
2013-10-12 10:34:00 +0800

    nand 串口 OK

--- u-boot-at91-u-boot-2012.10-at91/board/atmel/at91sam9263ek/at91sam9263ek.c
+++ u-boot-at91-u-boot-2012.10-at91/board/atmel/at91sam9263ek/at91sam9263ek.c
@@ -119,19 +119,19 @@ static void at91sam9263ek_macb_hw_init(void)
 	writel(1 << 25, &pio->pioc.pudr);
 	writel((1 << 25) | (1 <<26), &pio->pioe.pudr);
 
-	erstl = readl(&rstc->mr) & AT91_RSTC_MR_ERSTL_MASK;
-
-	/* Need to reset PHY -> 500ms reset */
-	writel(AT91_RSTC_KEY | AT91_RSTC_MR_ERSTL(0x0D) |
-		AT91_RSTC_MR_URSTEN, &rstc->mr);
-
-	writel(AT91_RSTC_KEY | AT91_RSTC_CR_EXTRST, &rstc->cr);
-	/* Wait for end hardware reset */
-	while (!(readl(&rstc->sr) & AT91_RSTC_SR_NRSTL))
-		;
-
-	/* Restore NRST value */
-	writel(AT91_RSTC_KEY | erstl | AT91_RSTC_MR_URSTEN, &rstc->mr);
+//	erstl = readl(&rstc->mr) & AT91_RSTC_MR_ERSTL_MASK;
+//
+//	/* Need to reset PHY -> 500ms reset */
+//	writel(AT91_RSTC_KEY | AT91_RSTC_MR_ERSTL(0x0D) |
+//		AT91_RSTC_MR_URSTEN, &rstc->mr);
+//
+//	writel(AT91_RSTC_KEY | AT91_RSTC_CR_EXTRST, &rstc->cr);
+//	/* Wait for end hardware reset */
+//	while (!(readl(&rstc->sr) & AT91_RSTC_SR_NRSTL))
+//		;
+//
+//	/* Restore NRST value */
+//	writel(AT91_RSTC_KEY | erstl | AT91_RSTC_MR_URSTEN, &rstc->mr);
 
 	/* Re-enable pull-up */
 	writel(1 << 25, &pio->pioc.puer);
